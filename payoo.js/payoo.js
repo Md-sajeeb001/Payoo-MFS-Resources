405e8935206117a75efc89bb31a2d43e1e345264
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-20 15:09:50 +0700

    added login form

--- payoo.js/payoo.js
+++ payoo.js/payoo.js
@@ -1,40 +1,14 @@
-document.getElementById('add-money-btn').addEventListener('click', function(event){
-
+document.getElementById('login-btn').addEventListener('click', function(event){
     event.preventDefault();
 
-    const mobNumber = getInputValueFieldById('mobile-number-field')
-    const pinNumber = getInputValueFieldById('pin-number')
+    const mobileInput = getInputValueById('mobile-number-field');
+    const pinInput = getInputValueById('pin-number');
 
-    if(typeof mobNumber !== "number" && typeof pinNumber !== "number"){
-        alert("Invalid Input")
+    if(pinInput === 1234 && mobileInput === 18){
+        window.location = '/home.html'
     }
-
-
-    if(pinNumber === 1234){
-        window.location = '/home.html';
+    else{
+        alert("log in faield")
     }
-    else {
-        alert('Please Provid a Valid Input');
-    }
-
-});
-
-document.getElementById('add-money-btn').addEventListener('click', function(){
-      const amountNumber = getInputValueFieldById('amount-number');
-      const pinNumber = getInputValueFieldById('pin-number');
-      const currentBalance = getTextValueFieldById('current-balance');
-      if(isNaN(amountNumber)){
-        alert('Invalid Input')
-        return;
-      }
-
-      if(pinNumber === 1234){
-        const addingAmount = amountNumber + currentBalance;
-        document.getElementById('current-balance').innerText = addingAmount;
-      }
-      else {
-        alert(`Sorry We Can't Adding Your Money`)
-      }
-
 
 })
--- payoo.js/uitilitis.js
+++ payoo.js/uitilitis.js
@@ -1,26 +1,5 @@
-function getInputValueFieldById(id){
-    const mobileNumber = document.getElementById(id).value;
-    const mobConNum = parseFloat(mobileNumber);
-    return mobConNum;
-}
-
-function getTextValueFieldById(id){
-    const currentBalance = document.getElementById(id).innerText;
-    const numberBalance = parseFloat(currentBalance);
-    return numberBalance;
-}
-
-// hidden section
-
-function getBtnHiddenValueById(id){
-    document.getElementById('addMoneyBtn').classList.add('hidden');
-    document.getElementById('addMoneyBtn').classList.add('CashOutBtn');
-    document.getElementById('addMoneyBtn').classList.add('transferBtn');
-    document.getElementById('addMoneyBtn').classList.add('bonusBtn');
-    document.getElementById('addMoneyBtn').classList.add('PaybillBtn');
-    document.getElementById('addMoneyBtn').classList.add('TransactionsBtn');
-
-    // show section
-
-    document.getElementById(id).classList.remove('hidden');
+function getInputValueById(id){
+    const getValue = document.getElementById(id).value;
+    const getNumberValue = parseFloat(getValue);
+    return getNumberValue;
 }
